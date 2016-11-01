7b5d047b869e8850f1e11b5ccce0a1d9ef3d1f8f
Adriano Godoy <godoy.ccp@gmail.com>
2016-11-01 13:49:11 -0200

    Coloca settings manualmente. Remove s3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ActiveRecord::Base
 	has_many :friendships, dependent: :destroy
 	has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id", dependent: :destroy
 
-	has_attached_file :avatar,
-					  :storage => :s3,
-					  :style => { :medium => "370x370", :thumb => "100x100" }
+	has_attached_file :avatar, :style => { :medium => "370x370", :thumb => "100x100" }
 
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
@@ -23,10 +21,10 @@ class User < ActiveRecord::Base
 		provider: auth['provider'],
 		uid: auth['uid'],
 		name: auth['info']['name'],
-		gender: auth['extra']['raw_info']['gender'],
-		date_of_birth: auth['extra']['raw_info']['birthday'],
-		location: auth['info']['location'],
-		bio: auth['extra']['raw_info']['bio']
+		# gender: auth['extra']['raw_info']['gender'],
+		# date_of_birth: auth['extra']['raw_info']['birthday'],
+		# location: auth['info']['location'],
+		# bio: auth['extra']['raw_info']['bio']
 
 
 		)
@@ -42,7 +40,7 @@ class User < ActiveRecord::Base
 	end
 
 	def remove_match(user2)
-		
+
 		inverse_friendship = inverse_friendships.where(user_id: user2).first
 
 		if inverse_friendship
@@ -61,7 +59,7 @@ class User < ActiveRecord::Base
 		case user.interest
 			when "Male"
 			where('gender = ?', 'male')
-			when "Female" 
+			when "Female"
 			where('gender = ?', 'female')
 			else
 			all
@@ -73,11 +71,11 @@ class User < ActiveRecord::Base
 	end
 
 	def matches(current_user)
-		friendships.where(state: "pending").map(&:friend) + current_user.friendships.where(state: "ACTIVE").map(&:friend) + current_user.inverse_friendships.where(state: "ACTIVE").map(&:user) 
+		friendships.where(state: "pending").map(&:friend) + current_user.friendships.where(state: "ACTIVE").map(&:friend) + current_user.inverse_friendships.where(state: "ACTIVE").map(&:user)
 	end
 	# Filter Methods
 
-	
+
 
 	private
 
@@ -86,5 +84,5 @@ class User < ActiveRecord::Base
 		avatar_url.scheme = 'https'
 		avatar_url.to_s
 	end
-	
+
 end
