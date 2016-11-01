d24a406e4d100a1707faab1efdabf20377b2c045
Oya <marciooya@gmail.com>
2016-11-01 14:50:18 -0200

    oya

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1796712163924803', 'd004145e5f18f28a0487431d2bd16ccd', { :scope => 'user_location, user_birthday, user_about_me, email'}
+  provider :facebook, '1796712163924803', 'd004145e5f18f28a0487431d2bd16ccd' #, { :scope => 'user_location, user_birthday, user_about_me, email'}
 end
